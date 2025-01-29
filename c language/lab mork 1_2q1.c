4bf01171db2a5c77f23ff19341e4e262e44a9c6e
Parth-M-M <parthmangroliya105@gmail.com>
2025-01-29 18:25:23 +0530

    lab work 1.2

---
+++ c language/lab mork 1_2q1.c
@@ -0,0 +1,10 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Name :- Parth \nAge :- 21\nSchool :- gec,Bharuch");
+
+    return 0;
+}
---
+++ c language/lab work1_2q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("--------\n");
+    printf("|\t|\n");
+    printf("R\t|\n");
+    printf("N\t|\n");
+    printf("W\t|\n");
+    printf("|\t|\n");
+    printf("--------\n");
+
+    return 0;
+}
