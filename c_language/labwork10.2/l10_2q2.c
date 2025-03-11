4962ad72aad08d302a7881be54d2bcdeee12a8e2
Parth-M-M <parthmangroliya105@gmail.com>
2025-03-11 16:03:32 +0530

    labwork

--- c_language/labwork10.2/l10_2q2.c
+++ c_language/labwork10.2/l10_2q2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int strcnt(char a[])
 {       int cnt = 0;
-    
-    for(int i=0;i<100;i++)
+    	int i ;
+    for(i=0;i<100;i++)
     {
         int cvt = a[i];
         if (cvt>=65&&cvt<=122)
---
+++ c_language/labwork11.1/l11_1q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int * ptr;
+    int n;
+    printf("Enter array size = ");
+    scanf("%d",&n);
+    int a[n];
+    ptr = a;
+    for(int i = 0 ; i < n ;i++)
+    {
+        printf("Enter Element a[%d]=",i);
+        scanf("%d",&a[i]);
+    }
+    printf("Square of each element is \n");
+    for(int i = 0 ; i < n ;i++)
+    {   int square  = (*ptr + i)*(*ptr + i); 
+        printf(" %d,",square);
+    }
+
+    return 0;
+}
