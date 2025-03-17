1a79419c2c10d247d4ff8b5e39eb4cb33958fffb
Parth-M-M <parthmangroliya105@gmail.com>
2025-03-17 16:38:03 +0530

    update

--- c_language/labwork12.1/l12_1q2.c
+++ c_language/labwork12.1/l12_1q2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Define the structure for Employee
 struct Employee {
     int emp_id;
     char emp_name[50];
@@ -15,49 +14,44 @@ int main() {
     int n;
     printf("Enter the number of employees: ");
     scanf("%d", &n);
-    getchar(); // Consume newline
     
     struct Employee employees[n];
     
-    // Input employee details
     for (int i = 0; i < n; i++) {
         printf("\nEnter details for Employee %d:\n", i + 1);
+        
         printf("Employee ID: ");
         scanf("%d", &employees[i].emp_id);
-        getchar(); // Consume newline
         
         printf("Name: ");
-        fgets(employees[i].emp_name, sizeof(employees[i].emp_name), stdin);
+        scanf(" %s", employees[i].emp_name);
         
         printf("Age: ");
         scanf("%d", &employees[i].emp_age);
-        getchar(); // Consume newline
         
         printf("Role: ");
-        fgets(employees[i].emp_role, sizeof(employees[i].emp_role), stdin);
+        scanf(" %s", employees[i].emp_role);
         
         printf("City: ");
-        fgets(employees[i].emp_city, sizeof(employees[i].emp_city), stdin);
+        scanf(" %s", employees[i].emp_city);
         
         printf("Experience (years): ");
         scanf("%d", &employees[i].emp_experience);
-        getchar(); // Consume newline
         
         printf("Company Name: ");
-        fgets(employees[i].emp_company_name, sizeof(employees[i].emp_company_name), stdin);
+        scanf(" %s", employees[i].emp_company_name);
     }
     
-    // Display employee details
     printf("\nEmployee Records:\n");
     for (int i = 0; i < n; i++) {
         printf("\nEmployee %d:\n", i + 1);
         printf("ID: %d\n", employees[i].emp_id);
-        printf("Name: %s", employees[i].emp_name);
+        printf("Name: %s\n", employees[i].emp_name);
         printf("Age: %d\n", employees[i].emp_age);
-        printf("Role: %s", employees[i].emp_role);
-        printf("City: %s", employees[i].emp_city);
+        printf("Role: %s\n", employees[i].emp_role);
+        printf("City: %s\n", employees[i].emp_city);
         printf("Experience: %d years\n", employees[i].emp_experience);
-        printf("Company Name: %s", employees[i].emp_company_name);
+        printf("Company Name: %s\n", employees[i].emp_company_name);
     }
     
     return 0;
