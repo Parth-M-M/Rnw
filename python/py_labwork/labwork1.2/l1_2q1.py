60d58c5678b5d9261f1333ff19732d63a3285980
Parth-M-M <parthmangroliya105@gmail.com>
2025-04-28 18:09:40 +0530

    labwork and labtest

---
+++ python/py_labwork/labwork1.2/l1_2q1.py
@@ -0,0 +1,3 @@
+print("parth","mangroliya","rnw","hello",sep="|")
+print(" hello there !!!",end=" ------> ")
+print("my name is parth mangroliya")
---
+++ python/py_labwork/labwork1.2/l1_2q2.py
@@ -0,0 +1,4 @@
+name = input("Enter Your name : ")
+age = int(input("Enter your age : "))
+hobby =input("Enter your hobby : ")
+print("Hello, my name is ",name," At ",age," ,enjoying ",hobby," sounds fun ! ")
